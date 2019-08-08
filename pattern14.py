7a13f0883185b6a24a630ef02ec2615178be28c1
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-08 21:32:19 +0530

    Create pattern14.py

---
+++ pattern14.py
@@ -0,0 +1,19 @@
+for i in range(3):
+    print("*",end="")
+    print(" "*7,end="")
+    print("*")
+print("* "*5)
+for i in range(3):
+    print("*",end="")
+    print(" "*7,end="")
+    print("*")
+
+""""
+*       *
+*       *
+*       *
+* * * * * 
+*       *
+*       *
+*       *
+"""
