7fd94dfb11d095da1e1c7c4a605d96532543aa0b
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-08 21:00:08 +0530

    Create pattern7.py

---
+++ pattern7.py
@@ -0,0 +1,25 @@
+print(" ",end="")
+for i in range(1,4):
+    print("*",end=" ")
+print()
+for i in range(2):
+    print("*",end="")
+    print(" "*5,end="")
+    print("*")
+print("* "*4)
+for i in range(3):
+    print("*",end="")
+    print(" "*5,end="")
+    print("*")
+
+
+
+"""
+ * * * 
+*     *
+*     *
+* * * * 
+*     *
+*     *
+*     *
+"""
