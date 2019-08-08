5c7aca54da4c5cdec5839a1499243b5df8fb7c69
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-08 21:17:38 +0530

    Create pattern10.py

---
+++ pattern10.py
@@ -0,0 +1,17 @@
+print("* "*4)
+for i in range(5):
+    print("* ",end="")
+    print(" "*5,end="")
+    print("*")
+print("* "*4,end="")
+
+
+"""
+* * * * 
+*      *
+*      *
+*      *
+*      *
+*      *
+* * * * 
+"""
