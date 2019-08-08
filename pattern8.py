d1852d64a29b52f55ea0869910942f86431be388
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-08 21:09:59 +0530

    Create pattern8.py

---
+++ pattern8.py
@@ -0,0 +1,23 @@
+print("* "*4)
+for i in range(2):
+    print("*",end="")
+    print(" "*6,end="")
+    print("*")
+print("* "*4)
+for i in range(2):
+    print("*",end="")
+    print(" "*6,end="")
+    print("*")
+print("* "*4)
+
+
+
+""""
+* * * * 
+*      *
+*      *
+* * * * 
+*      *
+*      *
+* * * * 
+"""
