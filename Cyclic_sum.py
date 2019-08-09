f9a85f758988a6b963697bd3998f5af15918130a
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-09 22:39:31 +0530

    Update Cyclic_sum.py

--- Cyclic_sum.py
+++ Cyclic_sum.py
@@ -10,3 +10,27 @@ def digsum(n):
             n=n//10
     return sum
 print(digsum(n))
+
+
+
+
+OR
+
+
+#include<isotream>  
+using namespace std; 
+  
+int digSum(int n) 
+{ 
+    if (n == 0)  
+       return 0; 
+    return (n % 9 == 0) ? 9 : (n % 9); 
+} 
+  
+// Driver program to test the above function 
+int main() 
+{ 
+    int n = 9999; 
+    cout<<digSum(n); 
+    return 0; 
+} 
