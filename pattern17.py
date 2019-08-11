95813fab4608318eeef030a1259b06e56a821e18
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-11 19:07:19 +0530

    Create pattern17.py

---
+++ pattern17.py
@@ -0,0 +1,19 @@
+for i in range(4,0,-1):
+    print("*",end="")
+    print(" "*i,end="")
+    print("*")
+for i in range(3):
+    print("*",end="")
+    print(" "*(i+2),end="")
+    print("*")
+
+
+"""
+*    *
+*   *
+*  *
+* *
+*  *
+*   *
+*    *
+"""
