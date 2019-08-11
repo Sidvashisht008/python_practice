ee43aef07c4acca9333f0b417cc9833c9384349e
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-11 19:55:15 +0530

    Create pattern21.py

---
+++ pattern21.py
@@ -0,0 +1,17 @@
+for row in range(7):
+    for col in range(7):
+        if((row==0 or row==6 or col==0 or col==6)and(row!=col)and abs(row-col)!=6):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+                               
+"""
+ ***** 
+*     *
+*     *
+*     *
+*     *
+*     *
+ ***** 
+"""
