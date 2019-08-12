e685c66c24527f124d3885ae61d225961f6485a9
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-12 22:06:57 +0530

    Update pattern26.py

--- pattern26.py
+++ pattern26.py
@@ -14,6 +14,28 @@ for i in A:
 98765432123456789 
 """
 
+n=int(input("enter series no."))
+a=n-1
+for i in range(1,n+1):
+    print(" "*a,end="")
+    for j in range(i,0,-1):
+        print(j,end="")
+    for j in range(2,i+1):
+        print(j,end="")
+    print()
+    a-=1
+
+"""
+        1         
+       212        
+      32123       
+     4321234      
+    543212345     
+   65432123456    
+  7654321234567   
+ 876543212345678  
+98765432123456789 
+"""
 
 
 
